f40be13c9cf799177778e259c0455b6627137ab0
Michał <mduzin@o2.pl>
2017-01-28 19:59:49 +0100

    new wave_file script

--- wave_file.py
+++ wave_file.py
@@ -3,32 +3,55 @@ import struct
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import time
+
  
  
-def DivideList(Spectrum,N,aggr_func):
-    
-    #metoda na przyspieszenie, pod warunkiem ze szerokosci przedzialow beda takie same
-    if 0==(len(Spectrum)%N):
-        return aggr_func(np.reshape(Spectrum,(N,len(Spectrum)/N)))
-    
-    BarRange = len(Spectrum)/N
-    BarSpectrum = []
- 
-    #czy mozna jakos zoptymalizowac Start i Stop Index'u???
-    StartIndex = 0.0
-    StopIndex = StartIndex + BarRange
-    for x in range(N):
-        BarSpectrum.append(Spectrum[round(StartIndex):round(StopIndex)])
-        StartIndex = StopIndex;
-        StopIndex =  StartIndex + BarRange
- 
-    BarSpectrum = [aggr_func(x) for x in BarSpectrum]
-    return BarSpectrum
- 
-#FileName = "WaveTest.wav"
-#FileName = "test.wav"
+#Zmienne globalne i Nastawy
+
+
+
+#Plik do analizy
 #FileName = "yeah.wav"
+#FileName = "test6.wav"
 FileName = "vega.wav"
+
+#Ilosc klatek na sek do wyswietlenia
+FPS = 16   #FPS-ilosc klatek na sekunde 
+
+#N - ilosc kolumn na wykresie spectrum 
+N = 16    
+
+#H - wyskosc slupka kolumny
+H = 8
+
+#Tablica z wyliczonymi widmami do wyswietlenia
+BarArray = []
+
+#zmienne pomocnicze
+BAR_ARRAY = np.fliplr(np.tril(np.ones((H+1,H),dtype=np.int),-1))
+
+#<TODO:> komentarz
+def PrepareBargraph(Spectrum):
+    Result = np.vstack((BAR_ARRAY[item] for item in Spectrum))
+    Result = np.packbits(Result, axis=0).flatten()
+    return Result 
+
+#<TODO:> komentarz    
+def DivideList(Spectrum,N):
+    #metoda na przyspieszenie, pod warunkiem ze szerokosci przedzialow beda takie same
+    Spectrum = np.reshape(Spectrum,(N,Spectrum.size/N))
+    return np.average(Spectrum, axis=1)  
+
+#<TODO:> komentarz    
+def ScaleSpectrum(Spectrum,OldMax,NewMax):
+    #Saturation filter
+    SatSpectrum = np.clip(Spectrum,0,OldMax)
+    #Scale array
+    ScaleSpectrum = np.array(SatSpectrum)
+    return np.around((ScaleSpectrum*NewMax)/OldMax)
+
+
 WaveObj = wave.open(FileName, mode='rb')
  
 print("Channels: ".ljust(25),WaveObj.getnchannels())
@@ -44,67 +67,76 @@ WaveParams = WaveObj.getparams(); #print("Params: ".ljust(25),WaveParams, "\n")
 #.wav file is always Little Endian so we use '<'
 FormatDict = {1:'B',2:'h',4:'i',8:'q'}
  
-#FPS          - ilosc klatek na sekunde 
-#WindowLength - number of frames we need to calculate current spectrum, eqaul to sampling frequency
-#WindowShift  - number of frames over which we will shift our signal in single iteration (half of WindowLength)
-#WaveData     - array of frame tuples from .wav file
-FPS = 23
-WindowLength = WaveParams.framerate
-WindowShift  = math.floor(WaveParams.framerate/FPS)
-WaveData = []
-WaveChannel = []
-Spectrum = []
- 
-for item in range(WaveParams.nchannels):
-    WaveChannel.append([0]*WindowLength)
-    Spectrum.append([])
+
+#FramesLength - number of frames we need to calculate current spectrum, eqaul to sampling frequency
+#FramesShift  - number of frames over which we will shift our signal in single iteration (half of WindowLength)
+#WaveData     - array of frames from .wav file
+
+#Chcemy żeby ilosc probek branych do liczenie widma byla wielokrotnoscia ilosci linii na wykresie
+FramesLength = WaveParams.framerate - WaveParams.framerate%N
+FramesShift  = math.floor(WaveParams.framerate/FPS)
+WaveData     = np.zeros(FramesLength)
     
  
 i=0
 while True:
-    WaveFrame = WaveObj.readframes(WindowShift)
-    if not WaveFrame: break
-    BarSpectrum = []
-    RealFrameNum = len(WaveFrame)//(WaveParams.sampwidth*WaveParams.nchannels)
-    WaveFrame = struct.unpack('<{n}{t}'.format(n=RealFrameNum*WaveParams.nchannels,t=FormatDict[WaveParams.sampwidth]),WaveFrame)
-    print(WaveFrame)
+    #pobieramy nowe ramki z pliku
+    WaveFrame = WaveObj.readframes(FramesShift)
     
-    for n in range(WaveParams.nchannels):
-        #isolate each channel 
-        WaveChannel[n] = WaveChannel[n][RealFrameNum:]
-        WaveChannel[n].extend([sample for (index,sample) in enumerate(WaveFrame) if (n == (index%WaveParams.nchannels))])
- 
-        #compute FFT for each channel
-        WaveChannelLen = len(WaveChannel[n])
-        Spectrum[n] = (2/WaveChannelLen)*np.fft.fft(WaveChannel[n])
-        Spectrum[n] = Spectrum[n][range(math.floor(WaveChannelLen/2))]
-        Spectrum[n] = abs(Spectrum[n])
- 
-        #teraz dzielimy nasze spectrum na N przedzialow
-        #N - ilosc przedzialow
-        #BarRange - ilosc prazakow czestotliwosci przypadajacych na pojedynczy przedzial
-        N=8
-        BarSpectrum.append(DivideList(Spectrum[n],N,max))
-        
- 
+    #jesli nie ma wiecej ramek to wyjdz z petli
+    if not WaveFrame: break
+
+    #Flow:
+    #1. stworz macierz numpy na ramki (inicuj zerami) DONE 
+    #2. odczyt nowej porcji danych tak jak jest: WaveFrame = WaveObj.readframes(FramesShift)DONE
+    #3. usun przestarzale ramki z poczatku .delete DONE
+    #4. dodanie odczytanej macierzy numpy z ramkami .append DONE
+    #5. rozdzial na kanalay .reshape DONE
+    #6. obliczanie rfft DONE
+    RealFramesLength = len(WaveFrame)//(WaveParams.sampwidth*WaveParams.nchannels)
+    WaveFrame = struct.unpack('<{n}{t}'.format(n=RealFramesLength*WaveParams.nchannels,t=FormatDict[WaveParams.sampwidth]),WaveFrame)
+    WaveData = np.delete(WaveData, np.s_[0:len(WaveFrame)], None)    
+    WaveData = np.append(WaveData,WaveFrame)
+    WaveChannel = np.array(WaveData).reshape(-1,WaveParams.nchannels)
+    
+    WaveChannel = np.delete(WaveChannel,-1,1) 
+    #liczymy rfft dla wszystkich kanalow
+    #<TODO:> obliczac tylko jeden kanal jesli ma byc realtime
+    Spectrum = np.abs(np.fft.rfft(WaveChannel,axis=0))
+    #nie wiem po co było to usuwanie ostatniego elementu(po nic tylko po to by ilosc ramek sie ladnie dzielila)
+    # ja usuwam pierwsza probke czyli 0Hz
+    Spectrum = np.sum(np.delete(Spectrum, 0, 0),axis=1)
+    #przejscie na decybele
+    Spectrum = np.log10(Spectrum)
+    
+    #teraz dzielimy nasze spectrum na N przedzialow
+    #N - ilosc przedzialow
+          
+    Spectrum = DivideList(Spectrum,N)
+    #<TODO:> Obmyslec lepszy sposob skalowania max'a
+    #Spectrum = ScaleSpectrum(Spectrum,5,H)
+    #Bargraph = PrepareBargraph(Spectrum)
+
     #rysuj/updatuj wykres widma
-    plt.figure(1)
-    plt.ylabel('Amplitude')
-    plt.xlabel('Czestoliwosc [Hz]')
-    plt.title('Widmo')
-    plt.ylim(0.0, 6000.0)
+    #plt.figure(1)
+    #plt.ylabel('Amplitude')
+    #plt.xlabel('Czestoliwosc [Hz]')
+    #plt.title('Widmo')
+    #plt.ylim(0.0, 8.0)
     #plt.plot(abs(Spectrum[0]),'r')
-    plt.bar(np.arange(N),BarSpectrum[0])
-    plt.grid(True)
-    plt.show()
+    #plt.bar(np.arange(N),Spectrum)
+    #plt.grid(True)
+    #plt.show() 
  
     print("Iter: ",i)
-    print("RealFrameNum: ",RealFrameNum )
-    print("len(WaveChannel[0]): ",len(WaveChannel[0]))
-    i += 1;
-    FramesNum = WindowShift
+    #print("Spectrum: ",Spectrum)
+    i += 1
     #break
-    
+
+ 
     
   
 print("Koniec czytania pliku .wav")
+
+
+print("Koniec skryptu")
